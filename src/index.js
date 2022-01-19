10ff7df8030cf5c538731867600872d0f8d50c50
ametrey <aetcheverryrey@gmail.com>
2022-01-19 14:49:40 -0300

    step 1 working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Title = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Statistics = ({text, counter}) => <p>{text} {counter}</p>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodFeedback = () => setGood(good + 1)
+  const handleNeutralFeedback = () => setNeutral(neutral + 1)
+  const handleBadFeedback = () => setBad(bad + 1)
+
+
+  return (
+    <div>
+      <Title text="Give feedback" />
+      <Button onClick={handleGoodFeedback} text="Good" />
+      <Button onClick={handleNeutralFeedback} text="Neutral" />
+      <Button onClick={handleBadFeedback} text="Bad" />
+      <Title text="Statistics" />
+    <Statistics text="Good" counter={good} />
+    <Statistics text="Neutral" counter={neutral} />
+    <Statistics text="Bad" counter={bad} />
+
+
+        </div>
+  )
+}
+
+ReactDOM.render(<App />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
